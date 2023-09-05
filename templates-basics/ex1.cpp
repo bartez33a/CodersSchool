a1f0ba6bd342e066234755db33afbf5fef3fa5c5
Bartosz Rozenkop <bartez33a@gmail.com>
2023-09-05 17:31:10 +0200

    added description for Templates - basics, exercise 1

--- templates-basics/ex1.cpp
+++ templates-basics/ex1.cpp
@@ -1,3 +1,10 @@
+/*
+   Create function makeComplex(), that creates complex number from two numbers. 
+   If their types are different, it should create complex number which type 
+   is the same as first parameter of function. 
+*/
+
+
 #include <iostream>
 #include <complex>
 
@@ -9,18 +16,16 @@ std::complex<T1> makeComplex(T1 first, T2 second)
 
 int main()
 {
-   std::cout << "Hello CMake!\n";
+   std::cout << "Templates - basics - exercise 1.\n";
 
    std::complex<int> a = makeComplex(4, 5);
-   std::cout << a.real() << ", " << a.imag() << '\n';
+   std::cout << "std::complex of type integer: " << a.real() << ", " << a.imag() << '\n';
 
    std::complex<double> b = makeComplex(3.1, 2.2);
-   std::cout << b.real() << ", " << b.imag() << '\n';
+   std::cout << "std::complex of type double: " << b.real() << ", " << b.imag() << '\n';
 
    std::complex<int> c = makeComplex(1, 5.1);
-   std::cout << c.real() << ", " << c.imag() << '\n';
-
-   std::cout << 1 << " " << double(1.1) << '\n';
+   std::cout << "std::complex of type integer: " << c.real() << ", " << c.imag() << '\n';
 
    return 0;
 }
