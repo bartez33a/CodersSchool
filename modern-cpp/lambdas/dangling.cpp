567d8af7afd38e074c74fc3e677e28b7a5cb5f26
Bartosz Rozenkop <bartez33a@gmail.com>
2023-10-14 18:09:36 +0200

    Added .clang_format to modern_cpp/lambdas added exercises: - dangling.cpp - homework - schedule

---
+++ modern-cpp/lambdas/dangling.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+auto getIndexGenerator() {
+    static int value = 0;
+    auto lambda = []() {
+        return value++;
+    };
+    return lambda;
+}
+
+int getFive() {
+    auto result = 5 + 4;
+    return result;
+}
+
+// Fix the code.
+// We want a generator to provide consecutive numbers starting from 0.
+// Correct output: `0123456789`
+
+int main() {
+    auto generator = getIndexGenerator();
+    [[maybe_unused]] int value = getFive();
+    for (int i = 0; i < 10; ++i) {
+        std::cout << generator();
+    }
+    std::cout << '\n';
+    return 0;
+}
---
+++ modern-cpp/lambdas/homework.cpp
@@ -0,0 +1,103 @@
+/*
+   1. create function schedule(), which takes two parameters:
+   -  first - any function "func" which takes no parameter and returns nothing
+   -  second - "duration" - time period in secnods (use library std::chrono).
+   Function schedule should wait "duration" seconds and then call function
+   "func".
+
+   2. Create overload of the function "schedule", which takes two arguments:
+      -  any function which takes int and returns nothing, and
+      -  int (instead of "duration").
+      How to pass int argument to this function?
+
+   3. Create another overload of schedule, which takes functions which take
+      std::string and double as their parameters and returns nothing.
+
+   Bonus: implement schedule as a templete function. One version will be enough
+   (without any overloads)!
+
+*/
+
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+/*
+   Inputs:
+   -  func     - pointer to function which takes no argument and reutrns nothing
+   -  duration - duration of time in seconds after which func is called
+*/
+void schedule(void (*func)(void), std::chrono::seconds duration) {
+  using namespace std::chrono_literals;
+
+  const auto start = std::chrono::steady_clock::now();
+  while (true) {
+    auto now = std::chrono::steady_clock::now();
+    auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
+    if (elapsed_seconds >= duration) {
+      break;
+    }
+  }
+
+  // call function
+  func();
+}
+
+void schedule(void (*func)(int), std::chrono::seconds duration, int func_arg) {
+  using namespace std::chrono_literals;
+
+  const auto start = std::chrono::system_clock::now();
+  while (true) {
+    auto now = std::chrono::system_clock::now();
+    auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
+    if (elapsed_seconds >= duration) {
+      break;
+    }
+  }
+
+  // call function
+  func(func_arg);
+}
+
+// Napisz kolejne przeciążenie, które będzie przyjmować funkcje,
+// które przyjmują std::string oraz double jako swoje argumenty i nic nie zwracają.
+void schedule(void (*func)(std::string, double), std::chrono::seconds duration, std::string text,
+              double value) {
+  using namespace std::chrono_literals;
+
+  const auto start = std::chrono::system_clock::now();
+  while (true) {
+    auto now = std::chrono::system_clock::now();
+    auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(now - start);
+    if (elapsed_seconds >= duration) {
+      break;
+    }
+  }
+
+  // call function
+  func(text, value);
+}
+
+int main() {
+  using namespace std::chrono_literals;
+
+  auto start = std::chrono::system_clock::now();
+  std::cout << "start!\n";
+  schedule([] { std::cout << "I'm an empty function\n"; }, 1s);
+  schedule([](int a) { std::cout << "Param int a = " << a << '\n'; }, 2s, 42);
+
+  schedule(
+      [](std::string s, double d) {
+        std::cout << "Params: string s  = " << s << ", double d = " << d << '\n';
+      },
+      0s, "text", 42.5);
+
+  auto stop = std::chrono::system_clock::now();
+
+  std::chrono::duration<double> diff = stop - start;
+  std::cout << "Everything took " << std::fixed << std::setprecision(6) << diff.count()
+            << " seconds\n";
+
+  return 0;
+}
